5139dd41b7f5b6601fd215e62a3bb18d66be94cc
ValeriyaShu95 <94042691+ValeriyaShu95@users.noreply.github.com>
2022-11-06 21:02:07 +0500

    Final2

--- src/com/hostel/Main.java
+++ src/com/hostel/Main.java
@@ -13,45 +13,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Bill persone1Bill = new Bill(2500);
-        Client persone1 = new Client("Max", "Shush", "+7999999999", "Max@mail.ru", "male");
+        Bill persone1Bill = new Bill(200);
+        Client persone1 = new Client("Max", "Shush", "+7999999999", "Max@mail.ru", "male", persone1Bill);
 
         Bill persone2Bill = new Bill(30000);
-        Client persone2 = new Client("Lera", "Shush", "+788888888", "lera@mail.com", "female");
+        Client persone2 = new Client("Lera", "Shush", "+788888888", "lera@mail.com", "female", persone2Bill);
 
 
-        Room [] hotel1Room = new Room[] {new Room(2, 2000, "free"), new Room (3,1000, "not free")};
+        Room[] hotel1Room = new Room[]{new Room(2, 2000, "free"), new Room(3, 1000, "not free")};
         Hotel hotel1 = new Hotel("Vegas", hotel1Room);
 
-        Room [] hotel2Room = new Room[] {new Room(1, 5000, "not free")}; //Все комнаты будут not free?
+        Room[] hotel2Room = new Room[]{new Room(1, 5000, "not free")}; //Все комнаты будут not free?
         Hotel hotel2 = new Hotel("Plasa", hotel2Room); // что это?
 
-        Room [] hotel3Room = new Room[] {new Room(5, 10000, "free")};
+        Room[] hotel3Room = new Room[]{new Room(5, 10000, "free")};
         Hotel hotel3 = new Hotel("Mariya", hotel3Room);
 
-        Room [] hotel4Room = new Room[] {new Room(7, 10500, "free")};
+        Room[] hotel4Room = new Room[]{new Room(7, 10500, "free")};
         Hotel hotel4 = new Hotel("Leroy", hotel4Room);
 
-        System.out.println("Выбирете комнату");
-        for (Room element: hotel1Room) {
-        System.out.println("1 - " + element.getPrice() + element.getPersoneAmount() + element.getFreeOrNot());
-    }
-        Scanner chooseRoom = new Scanner(System.in);
-        Integer ppp = chooseRoom.nextInt();
-
         Selected selected = new Selected();
-        Room selectedRoom = selected.selectedRoom(hotel1Room, ppp);
-
-
-
+        Room selectedRoom = selected.selectRoom(hotel1Room);
 
 
         Book book = new Book();
         BookingService booking = new BookingService();
         booking.Booking(hotel1, persone1, selectedRoom);
-
-
-
 
 
     }
--- src/com/hostel/service/BookingService.java
+++ src/com/hostel/service/BookingService.java
@@ -5,13 +5,27 @@ import com.hostel.entity.Hotel;
 import com.hostel.entity.Room;
 
 public class BookingService {
-    public void Booking (Hotel hotel, Client client, Room room) {  // почему Hotel hotel
-        System.out.println(client.getName() + "" + "Вы забронировали отель" + "" + hotel.getHotelName() + "" + "Ващ номер -" + "" + room.hashCode());
+    public void Booking(Hotel hotel, Client client, Room room) {  // почему Hotel hotel
 
+        boolean statusCheck = AmmountCheck(client.getBill(), room.getPrice());
+        if (statusCheck == true) {
+
+            Integer finaleBalance = client.getBill().getAmount() - room.getPrice();
+
+            System.out.println(client.getName() + " " + "Вы забронировали отель" + " " + hotel.getHotelName() + " " + "Ваш номер -" + " " + room.hashCode() + "\n"
+                    + "Остаток на вашем счете - " + " " + finaleBalance);
+        }
+        else
+            System.out.println("На вашем счете не достаточно денег");
 
     }
-    public void AmmountCheck (Bill bill  ){
 
+    public boolean AmmountCheck(Bill clientBill, Integer roomPrice) {
+        if (clientBill.getAmount() >= roomPrice) {
+            return true;
+        }
+        else
+
+        return false;
     }
-
 }
